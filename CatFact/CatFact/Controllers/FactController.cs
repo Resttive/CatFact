1e483bea4f0f3bb9e7e29fa8b32f133866f5dfd1
Karol <michkarol.2000@wp.pl>
2022-02-24 16:00:40 +0100

    Create FactController and view

---
+++ CatFact/CatFact/Controllers/FactController.cs
@@ -0,0 +1,20 @@
+﻿using CatFact.Data;
+using CatFact.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatFact.Controllers
+{
+    public class FactController : Controller
+    {
+        protected ICatFactReader _dataReader; 
+        public FactController(ICatFactReader dataReader)
+        {
+            _dataReader = dataReader;
+        }
+        public IActionResult Index()
+        {
+            FactCat fact = _dataReader.GetFact();
+            return View(fact);
+        }
+    }
+}
